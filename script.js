236feb38cbfaeb2fcb387c23a670b8a2777f4813
ZahraAlaali <zooz.alaali03@icloud.com>
2025-09-13 11:10:51 +0300

    modifysnakePosition

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let circlePosition = {
   x: null,
   y: null,
 }
-let direction
+let direction = "RIGHT"
 let active = false
 let current = 0
 let best = 0
@@ -37,17 +37,12 @@ const createBoard = () => {
   }
   allDivs = document.querySelectorAll("main div")
 }
-const placeSnake = () => {
-  //snake
-  for (let i = 0; i < snakePosition.length; i++) {
-    let place = snakePosition[i].y * 15 + snakePosition[i].x
-    allDivs[place].classList.add(i === 0 ? "snakeHead" : "snakeBody")
-  }
-}
 const circlePlace = () => {
   //circle
-  circlePosition.x = Math.floor(Math.random() * 15)
-  circlePosition.y = Math.floor(Math.random() * 15 + 1)
+  let x = Math.floor(Math.random() * 15)
+  let y = Math.floor(Math.random() * 15 + 1)
+  circlePosition.x = x
+  circlePosition.y = -1 * y + 15
   if (!active) {
     circle.style.width = "30px"
     circle.style.height = "30px"
@@ -56,15 +51,54 @@ const circlePlace = () => {
     document.querySelector("main").append(circle)
     circle.style.position = "relative"
   }
-  circle.style.top = `${circlePosition.y * -30}px`
-  circle.style.left = `${circlePosition.x * 30}px`
+  circle.style.top = `${y * -30}px`
+  circle.style.left = `${x * 30}px`
+  circle.style.visibility = "visible"
+}
+const placeSnake = () => {
+  //snake
+  console.log("place snake")
+  for (let i = 0; i < snakePosition.length; i++) {
+    console.log("place snake in loop")
+    let place = snakePosition[i].y * 15 + snakePosition[i].x
+    if (i === 0) {
+      allDivs[place].classList.add("snakeHead")
+    } else {
+      if (i === 1) {
+        allDivs[place].classList.remove("snakeHead")
+      }
+      allDivs[place].classList.add("snakeBody")
+    }
+  }
+}
+
+const modifySnakePosition = () => {
+  let place =
+    snakePosition[snakePosition.length - 1].y * 15 +
+    snakePosition[snakePosition.length - 1].x
+  // remove the tail
+  allDivs[place].classList.remove("snakeBody")
+  for (let i = snakePosition.length - 1; i >= 1; i--) {
+    snakePosition[i].x = snakePosition[i - 1].x
+    snakePosition[i].y = snakePosition[i - 1].y
+  }
+  if (direction === "RIGHT") {
+    snakePosition[0].x += 1
+  } else if (direction === "LEFT") {
+    snakePosition[0].x -= 1
+  } else if (direction === "UP") {
+    snakePosition[0].y -= 1
+  } else {
+    snakePosition[0].y += 1
+  }
+  placeSnake()
 }
 const frame = () => {
   if (
-    snakePosition[0].y <= 0 ||
-    snakePosition[0].y >= 16 ||
-    snakePosition[0].x <= 0 ||
-    snakePosition[0].x >= 16
+    snakePosition[0].y < 0 ||
+    snakePosition[0].y > 14 ||
+    snakePosition[0].x < 0 ||
+    snakePosition[0].x > 14
   ) {
     direction = null
     snakePosition = [
@@ -82,30 +116,24 @@ const frame = () => {
     current = 0
     currentSpan.innerText = current.toString()
   } else {
-    if (direction === "RIGHT") {
-      snakePosition[0].x += 1
-    } else if (direction === "LEFT") {
-      snakePosition[0].x -= 1
-    } else if (direction === "UP") {
-      snakePosition[0].y += 1
-    } else {
-      snakePosition[0].y -= 1
-    }
-    placeSnake()
+    console.log(snakePosition)
+    console.log(circlePosition)
     if (
       snakePosition[0].y === circlePosition.y &&
       snakePosition[0].x === circlePosition.x
     ) {
       //increase the size of the snake
+      snakePosition.push({ x: null, y: null })
       current += 1
       currentSpan.innerText = current.toString()
       if (current > best) {
         bestSpan.innerText = current.toString()
         best = current
       }
-      console.log(currentSpan)
+      circle.style.visibility = "hidden"
       circlePlace()
     }
+    modifySnakePosition()
   }
 }
 
@@ -115,29 +143,13 @@ document.addEventListener("keydown", (event) => {
     id = setInterval(frame, 300)
     active = true
   }
-  if (
-    event.code === "ArrowDown" &&
-    direction != "UP" &&
-    snakePosition[0].y != 1
-  ) {
+  if (event.code === "ArrowDown" && direction != "UP") {
     direction = "DOWN"
-  } else if (
-    event.code === "ArrowUp" &&
-    direction != "DOWN" &&
-    snakePosition[0].y != 15
-  ) {
+  } else if (event.code === "ArrowUp" && direction != "DOWN") {
     direction = "UP"
-  } else if (
-    event.code === "ArrowRight" &&
-    direction != "LEFT" &&
-    snakePosition[0].x != 14
-  ) {
+  } else if (event.code === "ArrowRight" && direction != "LEFT") {
     direction = "RIGHT"
-  } else if (
-    event.code === "ArrowLeft" &&
-    direction != "RIGHT" &&
-    snakePosition[0].x != 0
-  ) {
+  } else if (event.code === "ArrowLeft" && direction != "RIGHT") {
     direction = "LEFT"
   }
 })
